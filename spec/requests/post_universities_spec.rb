fb5661dd0731340c4a1231f377486195d8b5ec18
Asia Kane and Grady L Shelton <asia@>
2017-07-17 11:06:30 -0700

    add tests for post methods for universities

---
+++ spec/requests/post_universities_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'post universities', type: :request do
+  university = FactoryGirl.build(:university)
+
+  before {
+    post '/universities', params: {name: university.name, description: university.description, population: university.population, mascot: university.mascot }
+  }
+
+  it 'returns the university name' do
+    expect(JSON.parse(response.body)['name']).to eq university.name
+  end
+
+  it 'returns the university description' do
+    expect(JSON.parse(response.body)['description']).to eq university.description
+  end
+
+  it 'returns the university population' do
+    expect(JSON.parse(response.body)['population']).to eq university.population
+  end
+
+  it 'returns the university mascot' do
+    expect(JSON.parse(response.body)['mascot']).to eq university.mascot
+  end
+end

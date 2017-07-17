d3a763311c1f641918c48e65bfbf10241a21e066
Asia Kane and Grady L Shelton <grady@>
2017-07-17 11:25:27 -0700

    test invalid post exception

--- spec/requests/post_universities_spec.rb
+++ spec/requests/post_universities_spec.rb
@@ -23,3 +23,17 @@ describe 'post universities', type: :request do
     expect(JSON.parse(response.body)['mascot']).to eq university.mascot
   end
 end
+
+describe 'post exception handlers', type: :request do
+
+  university = FactoryGirl.build(:university)
+
+  before {
+    post '/universities', params: {name: '', description: university.description, population: university.population, mascot: university.mascot }
+  }
+
+  it 'returns error code 422' do
+    expect(response).to have_http_status :unprocessable_entity
+  end
+
+end

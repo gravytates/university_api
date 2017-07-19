b9dd8bb46d7a25f9520a5d8d5a1dbf929b9133ec
Asia Kane and Grady Shelton <asia@>
2017-07-19 16:43:04 -0700

    add downcase functionality to scope searches

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,12 +2,12 @@ class University < ApplicationRecord
   has_many :reviews
   validates :name, :description, :population, :mascot, presence: true
 
-  scope :search_by_name, -> (name) { where("name like ?", "%#{name}%") }
+  scope :search_by_name, -> (name) { where("lower(name) like ?", "%#{name.downcase}%") }
 
-  scope :search_by_description, -> (description) { where("description like ?", "%#{description}%") }
+  scope :search_by_description, -> (description) { where("lower(description) like ?", "%#{description.downcase}%") }
 
   scope :search_by_population, -> (population) { where("population = ?", population ) }
 
-  scope :search_by_mascot, -> (mascot) { where("mascot like ?", "%#{mascot}%") }
+  scope :search_by_mascot, -> (mascot) { where("lower(mascot) like ?", "%#{mascot.downcase}%") }
 
 end
